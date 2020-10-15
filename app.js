bd1603a899e9dfd493fd528b2a35f381ff302d4b
anubhav14g <anubhav1408sks@gmail.com>
2020-10-15 19:21:55 +0530

    few tweaks

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
